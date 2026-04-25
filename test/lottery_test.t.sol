7acc9ecd0c058ecb7fd07d0bef2bb509643b54aa
no-hive <no-hive@tutamail.com>
2026-04-25 10:23:55 +0300

    test layout planning complete

--- test/lottery_test.t.sol
+++ test/lottery_test.t.sol
@@ -1,5 +1,5 @@
 // 1. need to check if deploy runs correctly
-// 1.1.
+// 1.1. check addresses somehow + variables that are inscripted via contractor.
 
 // 2. let's check if lottery initialization works
 // call createAndStartLottery("test_name", 0) + payable + onlyowner
@@ -9,7 +9,7 @@
 // lotRewards == 0.1 ether;
 // contract balance == 0.1 ether;
 
-// let's check if lottery initizalition really should be payable
+// 3. let's check if lottery initizalition really should be payable
 // call createAndStartLottery("test_name", 0) + onlyowner
 // require
 // lotNonce == 0;
@@ -17,10 +17,27 @@
 // lotRewards == 0 ether;
 // contract balance == 0
 
-// lets check onlyonwer modifier perfomance=
+// 4. lets check onlyonwer modifier perfomance=
 // call createAndStartLottery("test_name", 0) + payable
 // should return (lotStarted = true);
 // lotNonce == 0;
 // lotMaxNonce == 0;
 // lotRewards == 0 ether;
 // contract balance == 0
+
+// 5. check all univaliable functions are really unavaliable.
+
+// 6. buy a ticket function loop. tries to get more tickets that are in Nonce.
+
+// 7. check that requestRandomWords works:
+// 7.1. call requestRandomWords
+// 7.2. add number in oracle contract
+// 7.3. check that this word is recieved in lottery contract
+
+// 8. revealRandomWinner
+
+// 9. try to take comissions - must be reverted.
+
+// 10. release rewards for winner.
+
+// 11. owner takes the comissions - must work in a nice way.
