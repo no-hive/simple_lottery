902345539e9d1a47de144183ea4ddad276e6175e
no-hive <no-hive@tutamail.com>
2026-04-29 23:41:14 +0300

    test practise

--- test/lottery_test.t.sol
+++ test/lottery_test.t.sol
@@ -1,13 +1,59 @@
+pragma solidity ^0.8.4;
+
+// import "forge-std/Test.sol";
+// import "../src/lottery.sol";
+
+// contract LotteryTest is Test {
+//    SimpleLottery simpleLottery;
+
+// these two ones are used for
+// 1. deployment
+// 2. CheckConstructorWorkedCorrectly test
+    uint256 immutable DEFAULT_SUBSCRIBTION_ID;
+    bytes32 immutable DEFAULT_KEYHASH;
+
+// these ones are used to initialise test lottery
+string memory _name = "HELLO_GAMBLERS"
+uint8 _maxTicketAmountOption = 1
+
+
+
+function setUp() public {
+        vRFCoordinatorV2_5Mock = new VRFCoordinatorV2_5Mock(_BASEFEE, _GASPRICELINK, _WEIPERUNITLINK);
+
+        address VRFCoordinatorV2_5Mock_address = address(vRFCoordinatorV2_5Mock);
+
+        uint256 _subid = vRFCoordinatorV2_5Mock.createSubscription();
+
+        vRFCoordinatorV2_5Mock.fundSubscription(_subid, 100000000000000000000);
+
+        simpleLottery = new SimpleLottery(_subid, VRFCoordinatorV2_5Mock_address, KEYHASH);
+
+        address SimpleLottery_address = address(simpleLottery);
+
+        vRFCoordinatorV2_5Mock.addConsumer(_subid, SimpleLottery_address);
+
+}
+
+
 // 1. need to check if deploy runs correctly
 // 1.1. check addresses somehow + variables that are inscripted via contractor.
 
-// 2. let's check if lottery initialization works
-// call createAndStartLottery("test_name", 0) + payable + onlyowner
-// should return (lotStarted = true);
-// lotNonce == 1;
-// lotMaxNonce == 10;
-// lotRewards == 0.1 ether;
-// contract balance == 0.1 ether;
+// function CheckConstructorWorkedCorrectly () {
+
+// assertEq(CONTRACT.s_subscriptionId(), DEFAULT_SUBSCRIBTION_ID);
+ // assertEq(CONTRACT.s_keyHash(), DEFAULT_KEYHASH);
+//}
+
+// function CheckLotteryInitializationWorks () {
+// assertEq (CONTRACT.createAndStartLottery(TEST_NAME, TEST_AMOUNT),(true, true, 1));
+// assertEq (CONTRACT.lotNonce(), 1);
+// assertEq (CONTRACT.lotMaxNonce(), 10);
+// assertEq (CONTRACT.lotRewards(), 0.1 ether);
+    // + mapping(uint256 => address) lotTicketsMapping;
+    // + contract balance == 0.1 ether;
+}
+
 
 // 3. let's check if lottery initizalition really should be payable
 // call createAndStartLottery("test_name", 0) + onlyowner
